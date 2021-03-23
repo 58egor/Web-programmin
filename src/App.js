b124158197c7cd28c41f3697fb12da57f56a4d4c
58egor <68480726+58egor@users.noreply.github.com>
2021-03-23 10:12:27 +0300

    lab2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import List from './List';
 function App() {
-  return (
-    <div className="App">
-        <h1>
-          Hello World
-        </h1>
-    </div>
-  );
-}
 
+const array = [
+{id: 1, content: `fistElement`},
+{id: 2, content: `secondElement`},
+{id: 3, content: `thirdElement`},
+];
+return (<header className="App-header">
+<List value={array}>
+</List> 
+</header>
+);
+}
 export default App;
